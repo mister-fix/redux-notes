32c2959d8ba5df7b0b0852de442c8f695dac8c45
Stephen <hellostephenwm@gmail.com>
2024-09-12 13:26:57 -0400

    added counter with redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { legacy_createStore as createStore } from "redux";
+
+const counterReducer = (state = 0, action) => {
+	switch (action.type) {
+		case "INCREMENT":
+			return state + 1;
+		case "DECREMENT":
+			return state - 1;
+		case "ZERO":
+			return 0;
+		default:
+			return state;
+	}
+};
+
+const store = createStore(counterReducer);
+
+const App = () => {
+	return (
+		<div>
+			<div>{store.getState()}</div>
+			<button onClick={() => store.dispatch({ type: "INCREMENT" })}>
+				plus
+			</button>
+			<button onClick={() => store.dispatch({ type: "DECREMENT" })}>
+				minus
+			</button>
+			<button onClick={() => store.dispatch({ type: "ZERO" })}>zero</button>
+		</div>
+	);
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+	root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
