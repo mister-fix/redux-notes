42e7804f3451f980b3edf0b8b9d50e049c22f758
Stephen <hellostephenwm@gmail.com>
2024-09-30 17:59:12 -0400

    wrapped application in react-redux provider, reorganized code into separate modules

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import App from "./App.jsx";
+import noteReducer from "./reducers/noteReducer";
+
+const store = createStore(noteReducer);
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</StrictMode>
 );
