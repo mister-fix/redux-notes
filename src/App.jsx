1aede2f0ed67ff3a32af0e71c86e7278fff3b2e2
Stephen <hellostephenwm@gmail.com>
2024-10-01 17:57:40 -0400

    imported and enabled the VisibilityFilter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
+import VisibilityFilter from "./components/VisibilityFilter";
 
 function App() {
 	const filterSelected = (value) => {
@@ -9,26 +10,7 @@ function App() {
 	return (
 		<div>
 			<NewNote />
-			<div>
-				all{" "}
-				<input
-					type="radio"
-					name="filter"
-					onChange={() => filterSelected("ALL")}
-				/>
-				important{" "}
-				<input
-					type="radio"
-					name="filter"
-					onChange={() => filterSelected("IMPORTANT")}
-				/>
-				nonimportant{" "}
-				<input
-					type="radio"
-					name="filter"
-					onChange={() => filterSelected("NONIMPORTANT")}
-				/>
-			</div>
+			<VisibilityFilter />
 			<Notes />
 		</div>
 	);
