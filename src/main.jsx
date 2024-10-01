72903d7468ba9977b283cfcbf36e0f04171a0c65
Stephen <hellostephenwm@gmail.com>
2024-10-01 19:33:02 -0400

    refactored code to use @reduxjs/toolkit configure store function

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 
 import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer.js";
 
-import { createNote } from "./reducers/noteReducer";
-import { filterChange } from "./reducers/filterReducer.js";
-
-const reducer = combineReducers({
-	notes: noteReducer,
-	filter: filterReducer,
+const store = configureStore({
+	reducer: {
+		notes: noteReducer,
+		filter: filterReducer,
+	},
 });
 
-const store = createStore(reducer);
-
 console.log(store.getState());
-
-store.subscribe(() => console.log(store.getState()));
-store.dispatch(filterChange("IMPORTANT"));
-store.dispatch(
-	createNote("combineReducers forms one reducer from many simple reducers")
-);
-
-/*createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</StrictMode>
-);*/
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={store}>
-			<App /> {/* Replaced empty div with the App component */}
+			<App />
 		</Provider>
 	</StrictMode>
 );
