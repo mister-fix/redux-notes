236d1b3dc6f2551074cd59708f5b662cd63f4938
Stephen <hellostephenwm@gmail.com>
2024-10-01 19:33:28 -0400

    modified reducer file to use 'createSlice' function of redux toolkit'

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 // Initial state
 const initialState = [
 	{
@@ -12,46 +14,32 @@ const initialState = [
 	},
 ];
 
-// Modified state to use initialState as default
-const noteReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case "NEW_NOTE": {
-			// modified to use array spread syntax
-			return [...state, action.payload]; // before: return state.concat(action.payload)
-		}
-		case "TOGGLE_IMPORTANCE": {
-			const id = action.payload.id;
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+const noteSlice = createSlice({
+	name: "notes",
+	initialState,
+	reducers: {
+		createNote(state, action) {
+			const content = action.payload;
+			state.push({
+				content,
+				important: false,
+				id: generateId(),
+			});
+		},
+		toggleImportanceOf(state, action) {
+			const id = action.payload;
 			const noteToChange = state.find((n) => n.id === id);
 			const changedNote = {
 				...noteToChange,
 				important: !noteToChange.important,
 			};
+
 			return state.map((note) => (note.id !== id ? note : changedNote));
-		}
-		default: {
-			return state;
-		}
-	}
-};
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
-export const createNote = (content) => {
-	return {
-		type: "NEW_NOTE",
-		payload: {
-			content,
-			important: false,
-			id: generateId(),
 		},
-	};
-};
+	},
+});
 
-export const toggleImportanceOf = (id) => {
-	return {
-		type: "TOGGLE_IMPORTANCE",
-		payload: { id },
-	};
-};
-
-export default noteReducer;
+export const { createNote, toggleImportanceOf } = noteSlice.actions;
+export default noteSlice.reducer;
