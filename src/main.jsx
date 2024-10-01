c4e87b800bca539b98a7c6336fd6e6fcbec74f38
Stephen <hellostephenwm@gmail.com>
2024-10-01 17:37:55 -0400

    modified code to use combineReducers hook from redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,42 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
-
 import App from "./App.jsx";
-import noteReducer from "./reducers/noteReducer";
 
-const store = createStore(noteReducer);
+import noteReducer from "./reducers/noteReducer";
+import filterReducer from "./reducers/filterReducer.js";
+
+import { createNote } from "./reducers/noteReducer";
+import { filterChange } from "./reducers/filterReducer.js";
+
+const reducer = combineReducers({
+	notes: noteReducer,
+	filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(filterChange("IMPORTANT"));
+store.dispatch(
+	createNote("combineReducers forms one reducer from many simple reducers")
+);
+
+/*createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</StrictMode>
+);*/
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={store}>
-			<App />
+			<div />
 		</Provider>
 	</StrictMode>
 );
