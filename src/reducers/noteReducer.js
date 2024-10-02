708090e0a20a63007fdae9eff3190e9f71f4f758
Stephen <hellostephenwm@gmail.com>
2024-10-02 17:38:57 -0400

    6-5: removed initial state array, added 'appendNote' action creator to slice

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,24 +1,10 @@
 import { createSlice, current } from "@reduxjs/toolkit";
 
-// Initial state
-const initialState = [
-	{
-		content: "reducer defines how redux store works",
-		important: true,
-		id: 1,
-	},
-	{
-		content: "state of store can contain any data",
-		important: false,
-		id: 2,
-	},
-];
-
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const noteSlice = createSlice({
 	name: "notes",
-	initialState,
+	initialState: [],
 	reducers: {
 		createNote(state, action) {
 			const content = action.payload;
@@ -40,8 +26,12 @@ const noteSlice = createSlice({
 
 			return state.map((note) => (note.id !== id ? note : changedNote));
 		},
+		// Add 'appendNote' action creator
+		appendNote(state, action) {
+			state.push(action.payload);
+		},
 	},
 });
 
-export const { createNote, toggleImportanceOf } = noteSlice.actions;
+export const { createNote, toggleImportanceOf, appendNote } = noteSlice.actions;
 export default noteSlice.reducer;
