c7ee51805d9d9dda99a99de60b6331c600fcc18f
Stephen <hellostephenwm@gmail.com>
2024-10-02 19:35:20 -0400

    6-5: moved initialization of notes to App component

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 
-import noteReducer, { setNotes } from "./reducers/noteReducer";
+import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer.js";
-import noteService from "./services/notes";
 
 const store = configureStore({
 	reducer: {
@@ -16,8 +15,6 @@ const store = configureStore({
 });
 
 console.log(store.getState());
-
-noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
