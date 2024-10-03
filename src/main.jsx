3606b517de5482c30daa79ccc9e3fd56e7c7e2c4
Stephen <hellostephenwm@gmail.com>
2024-10-03 14:03:49 -0400

    6-6: moved store configuration to own module

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import store from "./store";
 import App from "./App.jsx";
-
-import noteReducer from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer.js";
-
-const store = configureStore({
-	reducer: {
-		notes: noteReducer,
-		filter: filterReducer,
-	},
-});
 
 console.log(store.getState());
 
