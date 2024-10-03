6c5bce55a1b1b72ea6c0331bc4dce17df9344c73
Stephen <hellostephenwm@gmail.com>
2024-10-03 14:04:58 -0400

    6-6: modified new note creation to use the async createNote action creator instead of calling the server directly

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,18 +1,15 @@
 import { useDispatch } from "react-redux";
 import { createNote } from "../reducers/noteReducer";
-import noteService from "../services/notes";
 
 const NewNote = (props) => {
 	const dispatch = useDispatch();
 
+	// Modified method: removed newNote and call to noteService to create new note
 	const addNote = async (event) => {
 		event.preventDefault();
-
 		const content = event.target.note.value;
 		event.target.note.value = "";
-
-		const newNote = await noteService.createNew(content);
-		dispatch(createNote(newNote));
+		dispatch(createNote(content));
 	};
 
 	return (
